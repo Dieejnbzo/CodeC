8fa3405888bfcc05e511331f9fc2519c4ae9cdbf
Dieejnbzo <vutoandien12102003@gmail.com>
2022-12-06 00:22:58 +0700

    update code

--- test.c
+++ test.c
@@ -1,21 +1,31 @@
-#include <stdio.h>
-
+#include<stdio.h>
+void input(int *a, int *n){
+	while(*n<1||*n>100){
+		printf("Length of array: ");
+		scanf("%d", n);
+	}
+	for(int i=0; i<*n; i++){
+		printf("a[%d]=", i);
+		scanf("%d", &a[i]);
+	}
+}
+void appearance(int *a, int n, int x, int count){
+    count= 0;
+	for(int i=0; i<n; i++){
+		if(a[i]==x){
+			count+=1;
+		}
+	}
+}
+void output(int count){
+	printf("Appearance of x: %d", count);
+}
 int main(){
-    int i,j,m,n;
-    int arr[100][100];
-    printf("");
-    scanf("%d%d", &m,&n);
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            scanf("%d", &arr[i][j]);
-        }
-    }
-
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            printf("%5d ", arr[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
+	int a[100], n;
+	input(a, &n);
+	int x, count= 0;
+	printf("Enter value of x: ");
+	scanf("%d", &x);
+	appearance(a, n, x, count);
+	output(count);
 }
